5cec90cd276fbd2b864a3c69cb5425cd30fc841c
Muhammad Ilham Zakaria <65764215+ham-zettt@users.noreply.github.com>
2025-03-23 14:26:23 +0700

    Create test_modul_1.py

---
+++ tests/test_modul_1.py
@@ -0,0 +1,7 @@
+import pytest
+from modul_1.tugas_1 import hitung_luas_lingkaran
+
+def test_hitung_luas_lingkaran():
+    assert hitung_luas_lingkaran(7) == 153.93804002589985
+    assert hitung_luas_lingkaran(0) == 0.0
+    assert hitung_luas_lingkaran(1) == 3.141592653589793
