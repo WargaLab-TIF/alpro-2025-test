78e493c37d16c17de9efc4007cd530b09ca09f76
Muhammad Ilham Zakaria <65764215+ham-zettt@users.noreply.github.com>
2025-03-23 14:29:41 +0700

    Create tugas1.py

---
+++ modul1/tugas1.py
@@ -0,0 +1,8 @@
+import math
+
+def hitung_luas_lingkaran(jari_jari):
+    luas = math.pi * (jari_jari ** 2)
+    return luas
+
+# Contoh penggunaan:
+print(hitung_luas_lingkaran(7))  # Output: 153.93804002589985
